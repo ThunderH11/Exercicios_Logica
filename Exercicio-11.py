95b3147ecd00d2b8d82cbd55a59781a78551f231
Tânus Henrique <144248753+ThunderH11@users.noreply.github.com>
2023-10-04 23:01:44 -0300

    Exercicio 11 de arrey

--- Exercicio-11.py
+++ Exercicio-11.py
@@ -1,2 +1,11 @@
-for x in range(5):
-    print(x)
+numeros = [0] * 30
+for x in range(30):
+    numeros[x] = int(input('Digite um número: '))
+
+quantidade = 0
+num = int(input('Digite o número a ser pesquisado: '))
+for y in range(30):
+    if num == numeros[y]:
+        quantidade += 1
+
+print(f'O número {num} aparece {quantidade} vezes na lista')
