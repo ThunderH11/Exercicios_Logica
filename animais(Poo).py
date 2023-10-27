06468a68c470e173253cf8078dd723d01dac2ba6
Tânus Henrique <144248753+ThunderH11@users.noreply.github.com>
2023-10-27 16:36:12 -0300

    Animais(Poo)

---
+++ animais(Poo).py
@@ -0,0 +1,16 @@
+from Biblioteca import *
+
+joao = animal("curupira","branco")
+print(f"{joao.nome} {joao.cor}")
+
+mike = gato("mike","preto")
+mike.som()
+
+mila = cachoro("mila","branca")
+mila.som()
+
+kinho = coelho("kizinh","roxo")
+kinho.som()
+
+zinho = vaca("mimoza","preto e branco")
+zinho.som()
